2c18050a1efb4f4b042f3c95abd8fed53703fded
Sarah Carlisle <sarah@travelescaperoom.com>
2020-09-09 16:16:15 -0500

    done?

--- client.js
+++ client.js
@@ -1,3 +1,8 @@
+/*
+Jeff McMahon
+Kris Visness
+Sarah Carlisle
+*/
 const employees = [
   {
     name: 'Atticus',
@@ -40,4 +45,62 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+// let outputArray = [
+//   name: name,
+//   bonusPercentage: bonusPercentage,
+//   totalCompensation: totalCompensation,
+//   totalBonus: totalBonus
+// ];
+
+function bonus(employee){
+
+  let bonusObject = {
+    name: employee.name,
+    bonusPercentage: 0,
+    totalCompensation: 0,
+    totalBonus: 0
+  };
+
+  if( employee.reviewRating <= 2){
+      let totalCompensation = employee.annualSalary;
+      // outputArray.push(employee.name,  '0%', totalCompensation, (totalCompensation-employee.annualSalary))
+      bonusObject.bonusPercentage = 0;
+      bonusObject.totalCompensation = employee.annualSalary;
+      bonusObject.totalBonus = (employee.annualSalary * 0);
+      return bonusObject;
+    }
+    if ( employee.reviewRating === 3) {
+      let totalCompensation = (employee.annualSalary * 1.04);
+      bonusObject.bonusPercentage = '4%';
+      bonusObject.totalCompensation = totalCompensation;
+      bonusObject.totalBonus = (employee.annualSalary * 0.04);
+      return bonusObject;
+    }
+    if (employee.reviewRating === 4) {
+      let totalCompensation = (employee.annualSalary * 1.06);
+      bonusObject.bonusPercentage = '6%';
+      bonusObject.totalCompensation = totalCompensation;
+      bonusObject.totalBonus = (employee.annualSalary * 0.06);
+      return bonusObject;
+    }
+    if (employee.reviewRating === 5) {
+      let totalCompensation = (employee.annualSalary * 1.10);
+      bonusObject.bonusPercentage = '10%';
+      bonusObject.totalCompensation = totalCompensation;
+      bonusObject.totalBonus = (employee.annualSalary * 0.10);
+        return bonusObject;
+    }
+    if (employee.employeeNumber <= 9999) {
+      bonusObject.bonusPercentage = bonusObject.bonusPercentage += '5%');
+      bonusObject.totalCompensation = totalCompensation;
+      bonusObject.totalBonus = (bonusObject.totalBonus * 1.05)
+      return bonusObject;
+    }
+  }
+}
+
+for (i = 0; i < employees.length; i++) {
+  console.log((employee[i]));
+}
 console.log( employees );
+console.log(outputArray);
